8e315a689a8664f0d85bd4b6d38e521e30d4792d
Emi <graficoemiliano@gmail.com>
2021-05-12 17:45:54 -0400

    UPD

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,7 +5,32 @@ logicaBotones()
 modoOscuro()
 
 
+let pantallaCompleta = document.querySelector('.pantalla-completa')
+let documentElement = document.documentElement
 
+pantallaCompleta.addEventListener('click', () => {
+    if(pantallaCompleta.querySelector('svg').classList.contains('fa-compress') ) {
+        pantallaCompleta.querySelector('svg').classList.add('fa-expand')
+        pantallaCompleta.querySelector('svg').classList.remove('fa-compress') 
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
+    } else {
+        
+        if (documentElement.requestFullscreen)  documentElement.requestFullscreen()
+        else if (documentElement.webkitRequestFullscreen) documentElement.webkitRequestFullscreen()
+        else if (documentElement.msRequestFullscreen) documentElement.msRequestFullscreen()
+    
+        pantallaCompleta.querySelector('svg').classList.remove('fa-expand')
+        pantallaCompleta.querySelector('svg').classList.add('fa-compress') 
+    }
+
+
+})
 
 
 
